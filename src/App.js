6479d2dd0df6d813606c712059f3c17a110d2558
ALIAKSANDR KARLIUK <kasaleksandr1@gmail.com>
2023-03-07 18:55:53 +0300

    success

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function App() {
         geo.getCurrentPosition(successPosition, errorPosition)
 
     }, [data])
-    console.log(data)
+
     return (
         <div className="App">
             <h1>{data?.placeName || ''}</h1>
@@ -134,62 +134,23 @@ function App() {
                     <div>
                         <h2>{data?.nameOfEvent || ''}</h2>
                         {data?.forAdultsOnly && <span className={'adult'}>18+</span>}
+
                         <p>
-                            {/*<svg width="24" height="24" viewBox="0 0 24 24" fill="none"*/}
-                            {/*     xmlns="http://www.w3.org/2000/svg">*/}
-                            {/*    <path d="M8 2V5" stroke="#9B8978" strokeWidth="1.5" strokeMiterlimit="10"*/}
-                            {/*          strokeLinecap="round" strokeLinejoin="round"/>*/}
-                            {/*    <path d="M16 2V5" stroke="#9B8978" strokeWidth="1.5" strokeMiterlimit="10"*/}
-                            {/*          strokeLinecap="round" strokeLinejoin="round"/>*/}
-                            {/*    <path d="M3.5 9.09009H20.5" stroke="#9B8978" strokeWidth="1.5" strokeMiterlimit="10"*/}
-                            {/*          strokeLinecap="round" strokeLinejoin="round"/>*/}
-                            {/*    <path*/}
-                            {/*        d="M21 8.5V17C21 20 19.5 22 16 22H8C4.5 22 3 20 3 17V8.5C3 5.5 4.5 3.5 8 3.5H16C19.5 3.5 21 5.5 21 8.5Z"*/}
-                            {/*        stroke="#9B8978" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round"*/}
-                            {/*        strokeLinejoin="round"/>*/}
-                            {/*    <path d="M15.6947 13.7H15.7037" stroke="#9B8978" strokeWidth="2" strokeLinecap="round"*/}
-                            {/*          strokeLinejoin="round"/>*/}
-                            {/*    <path d="M15.6947 16.7H15.7037" stroke="#9B8978" strokeWidth="2" strokeLinecap="round"*/}
-                            {/*          strokeLinejoin="round"/>*/}
-                            {/*    <path d="M11.9955 13.7H12.0045" stroke="#9B8978" strokeWidth="2" strokeLinecap="round"*/}
-                            {/*          strokeLinejoin="round"/>*/}
-                            {/*    <path d="M11.9955 16.7H12.0045" stroke="#9B8978" strokeWidth="2" strokeLinecap="round"*/}
-                            {/*          strokeLinejoin="round"/>*/}
-                            {/*    <path d="M8.29431 13.7H8.30329" stroke="#9B8978" strokeWidth="2" strokeLinecap="round"*/}
-                            {/*          strokeLinejoin="round"/>*/}
-                            {/*    <path d="M8.29431 16.7H8.30329" stroke="#9B8978" strokeWidth="2" strokeLinecap="round"*/}
-                            {/*          strokeLinejoin="round"/>*/}
-                            {/*</svg>*/}
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12Z"
-                                    stroke="#9B8978" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                <path d="M15.7099 15.18L12.6099 13.33C12.0699 13.01 11.6299 12.24 11.6299 11.61V7.51001"
-                                      stroke="#9B8978" strokeWidth="1.5" strokeLinecap="round"
-                                      strokeLinejoin="round"/>
-                            </svg>
-                            {`Start: ${new Date(data?.calendarStartDate).toLocaleDateString().slice(0, 5)} ${new Date(data?.calendarStartDate).toLocaleTimeString().slice(0, 5)}`}
+                            <span className={'section--main-date-bold'}>Start:</span>
+                            <span>{` ${new Date(data?.calendarStartDate).toLocaleDateString().slice(0, 5)} at ${new Date(data?.calendarStartDate).toLocaleTimeString().slice(0, 5)}`}</span>
                         </p>
 
                         <p>
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12Z"
-                                    stroke="#9B8978" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                <path d="M15.7099 15.18L12.6099 13.33C12.0699 13.01 11.6299 12.24 11.6299 11.61V7.51001"
-                                      stroke="#9B8978" strokeWidth="1.5" strokeLinecap="round"
-                                      strokeLinejoin="round"/>
-                            </svg>
-                            {`End: ${new Date(data?.calendarEndDate).toLocaleDateString().slice(0, 5)} ${new Date(data?.calendarStartDate).toLocaleTimeString().slice(0, 5)}`}
+                            <span className={'section--main-date-bold'}>End:</span>
+                            <span>{`${new Date(data?.calendarEndDate).toLocaleDateString().slice(0, 5)} at ${new Date(data?.calendarStartDate).toLocaleTimeString().slice(0, 5)}`}</span>
                         </p>
+
 
                     </div>
                 </div>
 
                 <h4 style={{marginBottom: '10px'}}>Meeting description:</h4>
-                <p className={`${isMoreContent ? 'description-more' :'description'}`}>{data?.description}</p>
+                <p className={`${isMoreContent ? 'description-more' : 'description'}`}>{data?.description}</p>
                 {!isMoreContent && <p className={'read-more'} onClick={() => setIsMoreContent(true)}>Read more</p>}
 
                 <h4 style={{marginBottom: '10px'}}>Number of participants:</h4>
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCdm-NMWicucpA5orOV0ZqI-J13p3yRdaw",
-    authDomain: "meetformeet-358112.firebaseapp.com",
-    projectId: "meetformeet-358112",
-    storageBucket: "meetformeet-358112.appspot.com",
-    messagingSenderId: "104278876812",
-    appId: "1:104278876812:web:97e1572990ea488f6211a9",
-    measurementId: "G-FPFDM05X0Z"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
